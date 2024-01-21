1c4ea01b15540b026ca4388d048f4b4a71227978
Magdalena <49407485+madzikdek@users.noreply.github.com>
2024-01-21 14:27:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,96 +32,91 @@ def columns_generate(df):
     return columns
 
 
-def main():
-    path = 'data.csv'
-    # app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
-    app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
-    server = app.server
-    df = data_operations.import_csv(path)
-    df_temp = generate_random_table(df)
-    # ------------------------------------------------------------------------------
-    # App layout
-    app.layout = html.Center([
+path = 'data.csv'
+# app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
+app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+server = app.server
+df = data_operations.import_csv(path)
+df_temp = generate_random_table(df)
+# ------------------------------------------------------------------------------
+# App layout
+app.layout = html.Center([
 
-        html.H1("Get a game idea", style={'text-align': 'center'}),
+	html.H1("Get a game idea", style={'text-align': 'center'}),
 
-        dcc.Markdown('''
-        Bellow you can see a set of prompts that should help you come up with a game idea. 
-        Feel free to base your idea on favourite 3, random 4 or all of them.
-        The meaning of each field
-         '''),
-        html.Br(),
+	dcc.Markdown('''
+	Bellow you can see a set of prompts that should help you come up with a game idea. 
+	Feel free to base your idea on favourite 3, random 4 or all of them.
+	The meaning of each field
+	 '''),
+	html.Br(),
 
-        dcc.Markdown('''
-        Free Domain Character - a character who's story can inspire you, they can become main hero of your story, they can just lend you their world or anything else you can think of,
-        
-        Environment Type - where roughly your game will be taking place,
-        
-        Game Type - ...the type of game it will be... let's be honest this one is self-explanatory
-        
-        Established Title Mechanic - think of the game/series you got and take one element from them, it can be very specific (Cyberpunk-like ability to hack things) or very basic (limited inventory of Resident Evil),
-        
-        Theme - what will be the...general theme??.. again pretty self explanatory
-        '''),
-        html.Br(),
+	dcc.Markdown('''
+	Free Domain Character - a character who's story can inspire you, they can become main hero of your story, they can just lend you their world or anything else you can think of,
+	
+	Environment Type - where roughly your game will be taking place,
+	
+	Game Type - ...the type of game it will be... let's be honest this one is self-explanatory
+	
+	Established Title Mechanic - think of the game/series you got and take one element from them, it can be very specific (Cyberpunk-like ability to hack things) or very basic (limited inventory of Resident Evil),
+	
+	Theme - what will be the...general theme??.. again pretty self explanatory
+	'''),
+	html.Br(),
 
-        dcc.Markdown('''
-        Click the 'Generate' button to get a new set       
-        '''),
-        html.Br(),
+	dcc.Markdown('''
+	Click the 'Generate' button to get a new set       
+	'''),
+	html.Br(),
 
-        # dcc.Markdown('''
-        #         | title one | title two | title three | title four | title five |
-        #         |-----------|-----------|-------------|------------|------------|
-        #         | 1         | 2         | 3           | 4          | 5          |
-        #
-        #
-        #         '''),
+	# dcc.Markdown('''
+	#         | title one | title two | title three | title four | title five |
+	#         |-----------|-----------|-------------|------------|------------|
+	#         | 1         | 2         | 3           | 4          | 5          |
+	#
+	#
+	#         '''),
 
-        # dash_table.DataTable(df_temp.to_dict('records'), [{"name": i, "id": i} for i in df_temp.columns]),
-
-
-        # html.Div(id='output_container', children=[]),
-        # html.Br(),
-
-        # html.A(html.Button('Submit', id='submit-val', n_clicks=0),href='/
-        #html.Center([
-        dash_table.DataTable(
-            id='table',
-            data=[],#df.to_dict('records'),columns=[{"name": i, "id": i} for i in df.columns]
-            # fill_width=False
-            style_table = {'width': '80%','color': 'black'},
-            style_cell = {'text-align': 'center'}
-        ),
-        html.Br(),
-        html.Button(['Generate'], id='btn')  #])
-
-    ])
-
-    # ------------------------------------------------------------------------------
-    # Connect the Plotly graphs with Dash Components
-    @app.callback([Output("table", "data"),
-                   Output('table', 'columns')
-                   ],
-                  [Input(component_id='btn', component_property='n_clicks')]
-                  # [Output(component_id='output_container', component_property='children'),
-                  #  Output(component_id='my_bee_map', component_property='figure')],
-                  # [Input(component_id='slct_year', component_property='value')]
-                  )
-    def updateTable(clicked):
-
-        columns = columns_generate(df)
-        columns = [{"name": i, "id": i} for i in df.columns]
-        if clicked is None:
-            df_temp1 = generate_random_table(df)
-            return df_temp1.to_dict('records'), columns
-        df_temp1 = generate_random_table(df)
-        data=df_temp1#.values[0]
-        return data.to_dict('records') , columns
-    # df.to_dict('records')
-
-    app.run_server(debug=True)
+	# dash_table.DataTable(df_temp.to_dict('records'), [{"name": i, "id": i} for i in df_temp.columns]),
 
 
-if __name__ == '__main__':
-    main()
+	# html.Div(id='output_container', children=[]),
+	# html.Br(),
+
+	# html.A(html.Button('Submit', id='submit-val', n_clicks=0),href='/
+	#html.Center([
+	dash_table.DataTable(
+		id='table',
+		data=[],#df.to_dict('records'),columns=[{"name": i, "id": i} for i in df.columns]
+		# fill_width=False
+		style_table = {'width': '80%','color': 'black'},
+		style_cell = {'text-align': 'center'}
+	),
+	html.Br(),
+	html.Button(['Generate'], id='btn')  #])
+
+])
+
+# ------------------------------------------------------------------------------
+# Connect the Plotly graphs with Dash Components
+@app.callback([Output("table", "data"),
+			   Output('table', 'columns')
+			   ],
+			  [Input(component_id='btn', component_property='n_clicks')]
+			  # [Output(component_id='output_container', component_property='children'),
+			  #  Output(component_id='my_bee_map', component_property='figure')],
+			  # [Input(component_id='slct_year', component_property='value')]
+			  )
+def updateTable(clicked):
+
+	columns = columns_generate(df)
+	columns = [{"name": i, "id": i} for i in df.columns]
+	if clicked is None:
+		df_temp1 = generate_random_table(df)
+		return df_temp1.to_dict('records'), columns
+	df_temp1 = generate_random_table(df)
+	data=df_temp1#.values[0]
+	return data.to_dict('records') , columns
+# df.to_dict('records')
+
+app.run_server(debug=True)
